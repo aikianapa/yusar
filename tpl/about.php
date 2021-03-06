c1f735ab7082106729b1acff9dbb85f12c92e2ca
Blacker <oleg_frolov@mail.ru>
2021-03-06 22:50:00 +0300

    update

--- tpl/about.php
+++ tpl/about.php
@@ -4,17 +4,6 @@
     <wb-var bkg="header-2.jpg" />
     <wb-include wb-tpl="header.inc.php" />
     <div class="container mb-2r">
-
-<wb-multilang name="mlfld" wb-lang="en,ru,jp">
-<form>
-<input type="text" class="form-control" name="test">
-<wb-multiinput name="mulp">
-    <input name="m1">
-    <input name="m2">
-
-</wb-multiinput>
-</form>
-</wb-multilang>
     <h3>{{prop.data.ru.name}}</h3>
     </div>
     <wb-include wb-tpl="block-images.inc.php" />
