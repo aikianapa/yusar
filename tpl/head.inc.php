873a7967786c961d10502fa4094e9eedf2d5d959
Oleg Frolov <oleg_frolov@mail.ru>
2021-04-07 16:03:42 +0300

    update

--- tpl/head.inc.php
+++ tpl/head.inc.php
@@ -1,9 +1,13 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+
 <link rel="preload" href="/engine/lib/bootstrap/css/bootstrap.min.css" as="style">
 <link rel="preload" href="/tpl/assets/slick/slick-theme.css" as="style">
 <link rel="preload" href="/tpl/assets/slick/slick.min.css" as="style">
 <link rel="preload" href="/tpl/assets/css/style.less" as="style">
+<link rel="preload" href="/modules/earth/style.css" as="style" wb-if="id == 'home'">
 
 <link rel="preload" href="/engine/lib/bootstrap/js/bootstrap.min.js" as="script">
 <link rel="preload" href="/tpl/assets/slick/slick.min.js" as="script">
