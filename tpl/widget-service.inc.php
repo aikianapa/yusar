f7a4f7d967c558155ddbd5c3daa3fe26fe36b80a
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-15 21:31:51 +0700

    https://trello.com/c/Gq3TQBFh

--- tpl/widget-service.inc.php
+++ tpl/widget-service.inc.php
@@ -1,38 +1,28 @@
 <div class="pt-3r pb-3r widget-service">
-    <div class="container widget">
-        <div class="border-bottom pb-1r mb-2r">
-            <h3>Услуги</h3>
-        </div>
+  <div class="container widget">
+    <div class="border-bottom pb-1r mb-2r">
+      <h3>Услуги</h3>
+    </div>
     <div class="row">
-        <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top"width="254" height="164" data-src="/thumbc/254x164/src/tpl/assets/img/widget-holder.jpg" alt="">
-                    <div class="card-body">
-                        <p class="card-text">Сервис программного обеспечения</p>
-                    </div>
-                </div>
+      <div class="col-md-6">
+        <div class="card">
+          <img class="card-img-top" width="254" height="164"
+               data-src="/thumbc/254x164/src/tpl/assets/img/widget-holder.jpg" alt="">
+          <div class="card-body">
+            <p class="card-text">Сервис программного обеспечения</p>
+          </div>
         </div>
+      </div>
 
-        <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top"width="254" height="164" data-src="/thumbc/254x164/src/tpl/assets/img/widget-holder.jpg" alt="">
-                    <div class="card-body">
-                        <p class="card-text">Сервис оборудования</p>
-                    </div>
-                </div>
+      <div class="col-md-6">
+        <div class="card">
+          <img class="card-img-top" width="254" height="164"
+               data-src="/thumbc/254x164/src/tpl/assets/img/widget-holder.jpg" alt="">
+          <div class="card-body">
+            <p class="card-text">Сервис оборудования</p>
+          </div>
         </div>
-
-        <div class="col-md-4">
-                <div class="card">
-                    <img class="card-img-top"width="254" height="164" data-src="/thumbc/254x164/src/tpl/assets/img/widget-holder.jpg" alt="">
-                    <div class="card-body">
-                        <p class="card-text">Узнать больше</p>
-                    </div>
-                </div>
-        </div>        
-
+      </div>
     </div>
-
-
-    </div>
+  </div>
 </div>
