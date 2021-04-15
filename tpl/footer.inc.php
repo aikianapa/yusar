f60bcce31d1b12cfdd14bc67e038d599968815f4
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-15 17:14:36 +0700

    https://trello.com/c/oQ9u8fwz

--- tpl/footer.inc.php
+++ tpl/footer.inc.php
@@ -2,56 +2,113 @@
     <div class="container">
         <div class="row">
             <div class="col-md-3">
-                <ul class="navbar-nav navbar-nav--top" wb-tree="item=menu&branch=mainmenu->company">
+                <ul class="navbar-nav navbar-nav--top">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
+                        <a class="nav-link" href="">О Компании</a>
+                        <ul>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/about">Наша компания</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/history">Наша история</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/team">Наша команда</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/partners">Наши партнёры</a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
-                <ul class="navbar-nav" wb-tree="item=menu&branch=mainmenu->news">
+                <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <ul class="navbar-nav navbar-nav--top" wb-tree="item=menu&branch=mainmenu->products">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="col-md-3">
-                <ul class="navbar-nav navbar-nav--top" wb-tree="item=menu&branch=mainmenu->equipment">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav" wb-tree="item=menu&branch=mainmenu->service">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
+                        <a class="nav-link" href="/news">Новости</a>
                     </li>
                 </ul>
             </div>
             <div class="col-md-3">
-                <ul class="navbar-nav navbar-nav--top" wb-tree="item=menu&branch=mainmenu->projects">
+                <ul class="navbar-nav navbar-nav--top">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
+                        <a class="nav-link" href="">IT-продукты</a>
+                        <ul>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/mis">МИС</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/ris">РИС</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/paks">ПАКС</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/telemed">Телемедицина</a>
+                            </li>
+                        </ul>
                     </li>
                 </ul>
-                <ul class="navbar-nav" wb-tree="item=menu&branch=mainmenu->contacts">
+            </div>
+            <div class="col-md-3">
+                <ul class="navbar-nav navbar-nav--top">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{data.link}}">{{data.lang.{{_sess.lang}}}}</a>
+                        <a class="nav-link" href="">Оборудование и интеграция</a>
+                        <ul>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/equipment">Медицинское оборудование</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/system">Системная интеграция</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/complex">Комплексные инжинерные системы</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Услуги</a>
+                        <ul>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/service-software">Сервис программного обеспечения</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/service-equipment">Сервис оборудования</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-3">
+                <ul class="navbar-nav navbar-nav--top">
+                    <li class="nav-item">
+                        <a class="nav-link" href="">Проекты</a>
+                        <ul>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/map-presence">Карта присутствия</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/projects">Наши проекты</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/contacts">Контакты</a>
                     </li>
                 </ul>
             </div>
         </div>
+
         <div class="second-block pb-1r">
             <div class="address-block">
                 Адрес: <span>{{_sett.address}}</span>
             </div>
             <div class="info-block">
                 <div class="mr-25">
-                    <ul class="d-flex list-inline m-0 nobr" wb-tree="item=menu&branch=social&parent=false&children=false">
+                    <ul class="d-flex list-inline m-0 nobr"
+                        wb-tree="item=menu&branch=social&parent=false&children=false">
                         <li>
                             <a href="{{data.link}}" target="_blank">
                                 <svg width="24" height="24">
