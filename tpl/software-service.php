263bd944859b2bd13e0e58312532e1acd04521e4
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-16 15:02:15 +0700

    https://trello.com/c/0WjH0d36

---
+++ tpl/software-service.php
@@ -0,0 +1,36 @@
+<html lang="ru">
+<wb-include wb-tpl="head.inc.php" />
+
+<body class="loading">
+<wb-var bkg="header-4.jpg" />
+<wb-include wb-tpl="header.inc.php" />
+
+<div class="container pt-3r software-service">
+  <h3>{{_lang.title}}</h3>
+
+  <div class="row align-items-center software-service--content">
+    <div class="col-12 col-md-6">
+      <video />
+    </div>
+    <div class="col-12 col-md-6">
+      ЮСАР+ осуществляет поставку медицинского оборудования "под ключ", включая новейшие мобильные диагностические комплексы ПЭТ/КТ. Обеспечивает полный комплекс работ: от проектирования (стадия П и РД) до разработки и внедрения сложных информационных и автоматизированных систем
+    </div>
+  </div>
+
+  <div class="software-service--info">
+    Включая поставку, настройку и обьединение всего парка диагностического оборудования (ПЭТ, МРТ, КТ, УЗИ и т.д.), операционных, с подключением видеокамер из операционных, лабораторных установок.
+  </div>
+</div>
+
+<wb-include wb-tpl="submit-request.inc.php" />
+
+<wb-include wb-tpl="footer.inc.php" />
+</body>
+
+</html>
+<wb-lang>
+  [en]
+  title = Как мы работаем
+  [ru]
+  title = Как мы работаем
+</wb-lang>
