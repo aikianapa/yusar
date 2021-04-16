5fea56661f0c2672410a3c173e07d003a97b909a
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-16 11:14:29 +0700

    https://trello.com/c/9Sxn76sd & https://trello.com/c/ViwuHoIO

---
+++ tpl/comprehensive-engineering-solutions.php
@@ -0,0 +1,212 @@
+<html lang="ru">
+<wb-include wb-tpl="head.inc.php" />
+
+<body class="loading">
+<wb-var bkg="header-4.jpg" />
+<wb-include wb-tpl="header.inc.php" />
+
+<div class="container pt-3r comprehensive-engineering-solutions">
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/1.png" alt="">
+      <div>
+        Структурируемая кабельная система
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/2.png" alt="">
+      <div>
+        Локальная вычислительная сеть
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/3.png" alt="">
+      <div>
+        Система телефонной связи
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/4.png" alt="">
+      <div>
+        Система широковещательного телевидения и проводного радиовещания
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/5.png" alt="">
+      <div>
+        Система электрочасофикации
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/6.png" alt="">
+      <div>
+        Система палатной связи и сигнализации
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/7.png" alt="">
+      <div>
+        Система охранного телевизионного наблюдения
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/8.png" alt="">
+      <div>
+        Система контроля доступа
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/9.png" alt="">
+      <div>
+        Система охранного видеонаблюдения
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/10.png" alt="">
+      <div>
+        Система диспетчеризации инженерных систем
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/11.png" alt="">
+      <div>
+        Система пожарной сигнализации
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/12.png" alt="">
+      <div>
+        Система охранной сигнализация
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/13.png" alt="">
+      <div>
+        Системы оповещения и управления эвакуацией людей при пожаре
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/14.png" alt="">
+      <div>
+        Система связи в конференц-зале
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+  <div class="row comprehensive-engineering-solutions--items">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/comprehensive-engineering-solutions/15.png" alt="">
+      <div>
+        Автоматическая противопожарная защита
+        <a href="#">
+          Подробнее
+          <svg width="24" height="24" class="ml-10">
+            <image width="24" height="24" xlink:href="/tpl/assets/img/right-arrow.svg" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<wb-include wb-tpl="submit-request.inc.php" />
+
+<wb-include wb-tpl="footer.inc.php" />
+</body>
+
+</html>
---
+++ tpl/submit-request.inc.php
@@ -0,0 +1,42 @@
+<div class="container pb-3r widget submit-request-widget">
+  <h3 class="mt-0 pb-2r">{{_lang.feedback}}</h3>
+  <form method="post">
+    <div class="row">
+      <div class="col-6">
+        <input type="text" name="fullname" class="form-control" placeholder="{{_lang.fullname}}">
+      </div>
+      <div class="col-md-3">
+        <input type="phone" name="phone" class="form-control" placeholder="{{_lang.phone}}">
+      </div>
+      <div class="col-md-3">
+        <input type="email" name="email" class="form-control" placeholder="{{_lang.email}}">
+      </div>
+      <div class="col-12">
+        <textarea class="form-control" name="comment" rows="auto" placeholder="{{_lang.comment}}"></textarea>
+      </div>
+      <div class="col-12">
+        <a href="#" class="add-file pb-1r">Приложить документ до 10 мб.</a>
+      </div>
+      <div class="col-12">
+        <button type="button" class="btn btn-link">{{_lang.quotebtn}}</button>
+      </div>
+    </div>
+  </form>
+</div>
+
+<wb-lang>
+  [en]
+    feedback = Feedback
+    quotebtn = Get a Callback
+    fullname = Full name
+    phone = Phone
+    email = Your Email
+    comment = Comment
+  [ru]
+    feedback = Оставить заявку
+    quotebtn = Оставить заявку
+    fullname = Ф.И.О.
+    phone = Телефон
+    email = Ваш Email
+    comment = Комментарий
+</wb-lang>
