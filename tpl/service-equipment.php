e506f6dbcdd77084cfc53019d69f0e09e18fe620
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-16 11:41:32 +0700

    https://trello.com/c/953quYo7

---
+++ tpl/service-equipment.php
@@ -0,0 +1,66 @@
+<html lang="ru">
+<wb-include wb-tpl="head.inc.php" />
+
+<body class="loading">
+<wb-var bkg="header-4.jpg" />
+<wb-include wb-tpl="header.inc.php" />
+
+<div class="container pt-3r service-equipment">
+  <div class="row service-equipment--gallery">
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/service-equipment/1.png" alt="" />
+    </div>
+    <div class="col-12 col-md-6">
+      <img src="/tpl/assets/img/service-equipment/2.png" alt="" />
+    </div>
+  </div>
+
+  <div class="row service-equipment--info">
+    <div class="col-12">
+      <div class="service-equipment--info--title">
+        Команда сервисного отдела
+      </div>
+
+      <div class="service-equipment--info--ul">
+        <div>
+          Суммарный опыт инженеров более 30 лет, два инженера работают в компании с 1998 года
+        </div>
+        <div>
+          5 инженеров по стационарному ремонту, 3 специалиста по выездному сервису, 2 специалиста работают в филиалах
+        </div>
+        <div>
+          В 2003 году нами впервые в России успешно осуществлен ремонт ультразвукового эндоскопа
+        </div>
+        <div>
+          Более 1000 ремонтируемых и обслуживаемых приборов в год
+        </div>
+      </div>
+
+      <div class="service-equipment--info--title service-equipment--info--title__last">
+        Самый большой в России склад запасных частей для эндоскопов
+      </div>
+
+      <div class="service-equipment--info--ul">
+        <div>
+          Склад аппаратов для диагностики
+        </div>
+        <div>
+          Склад аппаратов, ожидающих запасные части
+        </div>
+        <div>
+          Склад аппаратов для ремонта
+        </div>
+        <div>
+          Аппараты после ремонта перемещаются на склад готовой продукции
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<wb-include wb-tpl="submit-request.inc.php" />
+
+<wb-include wb-tpl="footer.inc.php" />
+</body>
+
+</html>
