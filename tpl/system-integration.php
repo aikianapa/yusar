e91234a6cd101e37c874c44e8d242fca3d2cec99
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-16 11:14:29 +0700

    https://trello.com/c/9Sxn76sd & https://trello.com/c/ViwuHoIO

---
+++ tpl/system-integration.php
@@ -0,0 +1,45 @@
+<html lang="ru">
+<wb-include wb-tpl="head.inc.php" />
+
+<body class="loading">
+<wb-var bkg="header-4.jpg" />
+<wb-include wb-tpl="header.inc.php" />
+
+<div class="container pt-3r system-integration">
+  <div class="row system-integration--gallery">
+    <div class="col-12 col-md-3">
+      <img src="/tpl/assets/img/system-integration/1.png" alt="" />
+    </div>
+    <div class="col-12 col-md-3">
+      <img src="/tpl/assets/img/system-integration/2.png" alt="" />
+    </div>
+    <div class="col-12 col-md-3">
+      <img src="/tpl/assets/img/system-integration/3.png" alt="" />
+    </div>
+    <div class="col-12 col-md-3">
+      <img src="/tpl/assets/img/system-integration/4.png" alt="" />
+    </div>
+  </div>
+
+  <div class="row system-integration--info">
+    <div class="col-12">
+      <div>Аудит и предпроектное обследование сетей и информационной инфраструктуры предприятия;</div>
+      <div>Разработка и согласование необходимых документов;</div>
+      <div>Конфигурирование и поставка оптимального оборудования;</div>
+      <div>Аттестацию системы и ее отдельных компонентов;</div>
+      <div>Специальная проверка сложной техники на совместимость и отказоустойчивость;</div>
+      <div>Монтаж и ввод в эксплуатацию технических средств, системного программного обеспечения;</div>
+      <div>Ввод в эксплуатацию информационных систем, локально вычислительных сетей и активного оборудования;</div>
+      <div>Техническое сопровождение и поддержка;</div>
+      <div>Гарантийное и сервисное обслуживание;</div>
+      <div>Консультирование Заказчиков.</div>
+    </div>
+  </div>
+</div>
+
+<wb-include wb-tpl="submit-request.inc.php" />
+
+<wb-include wb-tpl="footer.inc.php" />
+</body>
+
+</html>
