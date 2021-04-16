896c81660bf73178ecb9e2bd08b0ef3cf792a2d1
Demidov Dmitriy <d.demidov@itmegastar.com>
2021-04-16 15:02:15 +0700

    https://trello.com/c/0WjH0d36

---
+++ tpl/licenses-and-certificates.php
@@ -0,0 +1,69 @@
+<html lang="ru">
+<wb-include wb-tpl="head.inc.php" />
+
+<body class="loading">
+<wb-var bkg="header-4.jpg" />
+<wb-include wb-tpl="header.inc.php" />
+
+<div class="container pt-3r pb-3r licenses-and-certificates">
+  <h3>{{_lang.title}}</h3>
+
+  <div class="slick">
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+1" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+2" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+3" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+4" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+5" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+6" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+7" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+8" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+9" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+10" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+11" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+12" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+13" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+14" alt="">
+    </div>
+    <div>
+      <img src="https://via.placeholder.com/160x200.png?text=Лицензия+15" alt="">
+    </div>
+  </div>
+</div>
+
+<wb-include wb-tpl="footer.inc.php" />
+</body>
+
+</html>
+<wb-lang>
+  [en]
+  title = Gallery
+  [ru]
+  title = Галерея
+</wb-lang>
