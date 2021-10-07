6dca7d75154632af15e02530aadf88abcc1945c9
ReinmandTrial <max.gluschtsov@gmail.com>
2021-10-07 18:15:40 +0300

    2 fix

--- tpl/home.php
+++ tpl/home.php
@@ -1,9 +1,16 @@
 <html lang="ru">
 <wb-include wb-tpl="head.inc.php" />
+
 <body class="loading">
     <wb-include wb-tpl="header.inc.php" class="homepage" />
-    
+
     <wb-include wb-tpl="widget-news.inc.php" />
+
+    <div class="container widget widget-products">
+        <div class="border-bottom pb-1r">
+            <h3>IT-Продукты</h3>
+        </div>
+    </div>
     <wb-include wb-tpl="widget-products.inc.php" class="bg-sky" />
     <wb-include wb-tpl="widget-equipment.inc.php" />
     <wb-include wb-tpl="widget-service.inc.php" class="bg-sky" />
@@ -12,4 +19,5 @@
     <wb-include wb-tpl="widget-projects.inc.php" />
     <wb-include wb-tpl="footer.inc.php" />
 </body>
+
 </html>
