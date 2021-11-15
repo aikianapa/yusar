c56db63e4a3408cb6c5890b84d0f9b4e684f7d94
Valeriy <talover9@gmail.com>
2021-11-15 20:22:02 +0200

    fix

--- tpl/home.php
+++ tpl/home.php
@@ -13,7 +13,7 @@
     </div>
     <wb-include wb-tpl="widget-products.inc.php" class="bg-sky" />
     <wb-include wb-tpl="widget-equipment.inc.php" />
-    <wb-include wb-tpl="widget-service.inc.php" class="bg-sky" />
+    <wb-include wb-tpl="widget-service.inc.php" class="bg-sky mb-0" />
     <wb-include wb-tpl="widget-globe.inc.php" />
 
     <wb-include wb-tpl="widget-projects.inc.php" />
