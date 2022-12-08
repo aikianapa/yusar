6677cd12ddde250f6e5555334d6336e39f17c4c5
Oleg Frolov <oleg_frolov@mail.ru>
2022-12-08 19:42:29 +0300

    update

--- forms/prodkeys/edit.php
+++ forms/prodkeys/edit.php
@@ -61,12 +61,12 @@
             <script>
                 (($)=>{
                     var generatePassword = function() {
-                        var length = 8,
+                        let length = 8,
                             charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
                             retVal = "";
-                        for (var i = 0, n = charset.length; i < length; ++i) {
-                            retVal += charset.charAt(Math.floor(Math.random() * n));
-                        }
+                            for (var i = 0, n = charset.length; i < length; ++i){
+                                retVal += charset.charAt(Math.floor(Math.random() * n));
+                            }
                         return retVal;
                     }
 
