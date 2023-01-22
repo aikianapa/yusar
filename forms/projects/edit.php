54469990e482ee922a152257452b7a25ecc9a464
Oleg Frolov <oleg_frolov@mail.ru>
2023-01-22 16:39:48 +0300

    update

--- forms/projects/edit.php
+++ forms/projects/edit.php
@@ -11,67 +11,66 @@
     $dom->find('.modal-dialog')->addClass('modal-xxl');
     " />
 </wb-include>
-    <wb-jq wb="html=.modal-body">
-                <div class="row">
-                    <div class="col-5">
-                        <form id="{{_form}}EditForm">
-                            <div>
-                                <div class="form-group row mb-1">
+<wb-jq wb="html=.modal-body">
+    <div class="row">
+        <div class="col-5">
+            <form id="{{_form}}EditForm">
+                <div>
+                    <div class="form-group row mb-1">
 
-                                    <div class="col-12">
-                                        <div class="divider-text my-0">Обложка</div>
-                                        <wb-module wb="module=filepicker&mode=single&width=800&&height=300"
-                                            wb-path="/uploads/projects" name="cover">
-                                        </wb-module>
-                                    </div>
+                        <div class="col-12">
+                            <div class="divider-text my-0">Обложка</div>
+                            <wb-module wb="module=filepicker&mode=single&width=800&&height=300" wb-path="/uploads/projects" name="cover">
+                            </wb-module>
+                        </div>
 
-                                    <div class="col">
-                                        <input name="header" class="form-control" placeholder="Заголовок"
-                                            wb="module=langinp" required>
-                                    </div>
-                                    <div class="col-auto">
-                                        <input name="active" wb-module="swico" data-size="36">
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-1">
-                                    <div class="col-12">
-                                        <textarea name="text" rows="auto" class="form-control" placeholder="Текст" wb-module="langinp"></textarea>
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-1">
-                                    <label class="col-sm-6">Год проекта</label>
-                                    <div class="col-sm-6">
-                                        <input type="yearpicker" name="year" class="form-control" wb="module=datetimepicker">
-                                    </div>
-                                </div>
-                                <div class="form-group row mb-1">
-                                    <div class="col-sm-6">
-                                        <label>Тип проекта</label>
-                                        <select name="type" wb-select2>
-                                            <option value="r">Российский</option>
-                                            <option value="m">Международный</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-sm-6">
-                                        <label>Категория</label>
-                                        <select name="category" wb-select2 wb-tree="dict=project_category&tpl=false">
-                                            <option value="{{id}}">{{name}}</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <wb-module wb="module=yonger&mode=structure&preset=projects" />
-                        </form>
+                        <div class="col">
+                            <input name="header" class="form-control" placeholder="Заголовок" wb="module=langinp" required>
+                        </div>
+                        <div class="col-auto">
+                            <input name="active" wb-module="swico" data-size="36">
+                        </div>
                     </div>
-
-                    <div class="col-7">
-                        <div id="yongerBlocksForm">
-                            <form method="post">
-
-                            </form>
+                    <div class="form-group row mb-1">
+                        <div class="col-12">
+                            <textarea name="text" rows="auto" class="form-control" placeholder="Текст" wb-module="langinp"></textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row mb-1">
+                        <label class="col-sm-6">Год проекта</label>
+                        <div class="col-sm-6">
+                            <input type="yearpicker" name="year" class="form-control" wb="module=datetimepicker">
+                        </div>
+                    </div>
+                    <div class="form-group row mb-1">
+                        <div class="col-sm-6">
+                            <label>Тип проекта</label>
+                            <select name="type" wb-select2>
+                                <option value="r">Российский</option>
+                                <option value="m">Международный</option>
+                            </select>
+                        </div>
+                        <div class="col-sm-6">
+                            <label>Категория</label>
+                            <select name="category" wb-tree="dict=project_category&tpl=false" wb-select2>
+                                <option value="{{id}}">{{name}}</option>
+                            </select>
                         </div>
                     </div>
                 </div>
+                <wb-module wb="module=yonger&mode=structure&preset=projects" />
+            </form>
+        </div>
 
-    </wb-jq>
+        <div class="col-7">
+            <div id="yongerBlocksForm">
+                <form method="post">
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</wb-jq>
+
 </html>
--- tpl/projects-show.php
+++ tpl/projects-show.php
@@ -2,44 +2,44 @@
 <wb-include wb-tpl="head.inc.php" />
 
 <body class="loading">
-<wb-var bkg="header-4.jpg" />
-<wb-include wb-tpl="header.inc.php" />
+  <wb-var bkg="header-4.jpg" />
+  <wb-include wb-tpl="header.inc.php" />
 
-<style>
-  .jumbotron {
-    margin-top: 9rem !important;
-  }
-
-  @media (max-width: 767px) {
+  <style>
     .jumbotron {
-      margin-top: 64px !important;
+      margin-top: 9rem !important;
     }
-  }
 
-  @media (max-width: 375px) {
-    .jumbotron h1 {
-      font-size: 28px !important;
-      line-height: 40px !important;
+    @media (max-width: 767px) {
+      .jumbotron {
+        margin-top: 64px !important;
+      }
     }
-  }
-</style>
 
-<div class="container pt-3r project">
+    @media (max-width: 375px) {
+      .jumbotron h1 {
+        font-size: 28px !important;
+        line-height: 40px !important;
+      }
+    }
+  </style>
 
-  <div class="row align-items-start">
-    <div class="col-12 col-md-8">
-      <h3 class="mt-0">{{header}}</h3>
+  <div class="container pt-3r project">
 
-      <p>{{text}}</p>
+    <div class="row align-items-start">
+      <div class="col-12 col-md-8">
+        <h3 class="mt-0">{{header}}</h3>
 
-      
-      <div class="image__container">
-        <img src="/thumbc/730x395/src{{cover.0.img}}" alt="">
-      </div>
-      <div>
-      <wb-module wb="module=yonger&mode=render" />
-      </div>
-<!--
+        <p>{{text}}</p>
+
+
+        <div class="image__container" wb-if="'{{cover.0.img}}' > ''">
+        <img src=" /thumbc/730x395/src{{cover.0.img}}" alt="">
+        </div>
+        <div>
+          <wb-module wb="module=yonger&mode=render" />
+        </div>
+        <!--
       <h3>Отзывы и благодарности</h3>
 
       <div class="slick">
@@ -63,37 +63,37 @@
         </div>
       </div>
 -->
-    </div>
-    <div class="col-12 col-md-4">
-      <div class="project--sidebar">
-        <div class="project--sidebar--title">
-          {{_lang.others}}
-        </div>
-        <wb-lang>
-          [ru]
-          others = "Другие проекты"
-          [en]
-          others = "Other projects"
-        </wb-lang>
-        <wb-foreach wb="table=projects&limit=5&rand=true&tpl=false" wb-filter="active=on&id!={{id}}">
-        <div class="project--sidebar--item">
-          <a href="/project" class="d-flex">
-            <img src="/thumbc/60x60/src{{cover.0.img}}" alt="">
-            <div>{{header}}</div>
+      </div>
+      <div class="col-12 col-md-4">
+        <div class="project--sidebar">
+          <div class="project--sidebar--title">
+            {{_lang.others}}
+          </div>
+          <wb-lang>
+            [ru]
+            others = "Другие проекты"
+            [en]
+            others = "Other projects"
+          </wb-lang>
+          <wb-foreach wb="table=projects&limit=5&rand=true&tpl=false" wb-filter="active=on&id!={{id}}">
+            <div class="project--sidebar--item">
+              <a href="/project" class="d-flex">
+                <img src="/thumbc/60x60/src{{cover.0.img}}" alt="">
+                <div>{{header}}</div>
+              </a>
+            </div>
+          </wb-foreach>
+          <a href="/projects" class="link-more-detailed bold">
+            Все проекты
+            <div class="arrow-mask"></div>
           </a>
-        </div>
-        </wb-foreach>
-        <a href="/projects" class="link-more-detailed bold">
-          Все проекты
-          <div class="arrow-mask"></div>
-        </a>
 
+        </div>
       </div>
     </div>
   </div>
-</div>
 
-<wb-include wb-tpl="footer.inc.php" />
+  <wb-include wb-tpl="footer.inc.php" />
 </body>
 
 </html>
