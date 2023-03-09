4ec0e5574ee64bc1a5507eddd1821b44533c072e
Oleg Frolov <oleg_frolov@mail.ru>
2023-03-09 10:14:28 +0300

    widget-history

--- blocks/widget-history.php
+++ blocks/widget-history.php
@@ -1,17 +1,22 @@
 <edit header="История">
     <wb-include wb-src="/engine/modules/yonger/common/blocks/common.inc.php" />
     <wb-multiinput name="history">
-        <div class="col-3">
-            <input class="form-control" type="text" name="year" placeholder="Год">
-            <wb-module wb="module=filepicker&mode=single&width=150&height=100" name="image" />
+        <div class="col-12">
+            <wb-module wb="module=filepicker&width=150&height=100" name="image" />
         </div>
-        <div class="pl-3 col-9">
-                <div class="col-12">
+        <div class="pl-3 col-12">
+            <div class="mb-2 row">
+                <div class="mt-1 col-sm-2">
+                    <input class="form-control" type="text" name="year" placeholder="Год">
+                </div>
+                <div class="mt-1 col-sm-10">
                     <input class="form-control" type="text" name="title" placeholder="Заголовок" wb-module="langinp">
                 </div>
-                <div class="col-12">
+                <div class="mt-1 col-12">
                     <textarea class="form-control" type="text" name="text" rows="auto" placeholder="Текст" wb-module="langinp"></textarea>
                 </div>
+
+            </div>
         </div>
     </wb-multiinput>
 
@@ -20,7 +25,7 @@
 
 <view>
 
-<section id="History">
+    <section id="History">
         <div class="row">
             <div class="col-lg-6 dst-data">
                 <h1 class="mt-3r year">{{history.0.year}}</h1>
@@ -30,54 +35,52 @@
             <div class="col-lg-6">
                 <div class="row" style="height:100%;">
                     <div class="col-3 pt-3r dates">
-                        <div class="slick-uninit slick-nav">
+                        <div class="text-center">
                             <wb-foreach wb="from=history">
-                                <div class="d-block slick-slide" data-year="{{year}}">{{year}}</div>
+                                <div class="pb-1 cursor-pointer d-block" data-year="{{year}}">{{year}}</div>
+                                <div class="d-none src-data" data-year="{{year}}" data-title="{{title.{{_sess.lang}}}}">{{text.{{_sess.lang}}}}</div>
                             </wb-foreach>
                         </div>
                     </div>
-                    <div class="col-9" style="padding-left: 0 !important;">
-                        <div class="slick slick-for dates-slider" data-options="vertical=true&verticalSwiping=true&dots=false&slidesToShow=1&slidesToScroll=1&responsive=false&dots=false&infinite=true&arrows=false&adaptiveHeight&true">
-                            <wb-foreach wb="from=history">
-                                <div>
-                                    <wb-var next="{{_parent.history.{{_ndx}}.year}}" wb-if="'{{_parent.history.{{_ndx}}.year}}'>''" else="{{_parent.history.0.year}}" />
-                                    <div data-year="{{year}}">
-                                      <img class="img-fluid" width="660" height="480" src="/thumbc/655x480/src/{{_parent.history.{{_ndx}}.image.0.img}}" alt="{{year*1 + 1}}"  wb-if="'{{_parent.history.{{_ndx}}.image.0.img}}'>''">
-                                      <img class="img-fluid" width="660" height="480" src="/thumbc/655x480/src/{{_parent.history.0.image.0.img}}" alt="{{year*1 + 1}}"  wb-if="'{{_parent.history.{{_ndx}}.image.0.img}}'==''">
+                    <div class="col-9 slides">
+                        <wb-foreach wb="from=history&tpl=false">
+                            <div class="year-block d-none" data-year="{{year}}">
+                                <wb-foreach wb="from=image&tpl=false">
+                                    <div wb-if="'{{_idx}}'=='0'" data-year="{{_parent.year}}">
+                                        <img class="img-fluid" width="660" height="480" src="/thumbc/655x480/src/{{img}}" alt="">
                                     </div>
-                                    <div class="d-none src-data" data-year="{{year}}" data-title="{{title.{{_sess.lang}}}}">{{text.{{_sess.lang}}}}</div>
                                     <div>
-                                        <img class="img-fluid" width="660" height="480" src="/thumbc/655x480/src/{{image.0.img}}" alt="{{year}}">
+                                        <img class="img-fluid" width="660" height="480" src="/thumbc/655x480/src/{{img}}" alt="">
                                     </div>
-                                </div>
-                            </wb-foreach>
-                        </div>
+                            </div>
+                        </wb-foreach>
                     </div>
+                    </wb-foreach>
                 </div>
             </div>
         </div>
+        </div>
         <script wb-app remove>
-            let $src = $('#History .slick div.src-data:first');
-            let $dst = $('#History div.dst-data');
-            //$dst.children('.year').html($src.data('year'));
-            //$dst.children('.title').html($src.data('title'));
-            //$dst.children('.text').html($src.html());
-            $('#History .dates [data-year]').off('click');
-            $('#History .dates [data-year]').on('click',function(){
+            $('#History .year-block:eq(0)').removeClass('d-none')
+            $('#History .dates [data-year]').click(function() {
+                let $src = $(this).next('src-data');
+                let $dst = $('#History div.dst-data');
+                let year = $(this).data('year')
+                $('#History .year-block').addClass('d-none')
+                $(`#History .year-block[data-year=${year}]`).removeClass('d-none')
                 let timeout = 200;
                 let that = this;
-                $dst.hide(timeout);
-                setTimeout(function(){
+                $dst.fadeOut(timeout);
+                setTimeout(function() {
                     let year = $(that).data('year');
-                    let $src = $('#History div.src-data[data-year='+year+']');
+                    let $src = $('#History div.src-data[data-year=' + year + ']');
                     $dst.children('.year').html($src.data('year'));
                     $dst.children('.title').html($src.data('title'));
                     $dst.children('.text').html($src.html());
-                    $dst.show(timeout);
-                },timeout)
-
-            });
+                    $dst.fadeIn(timeout);
+                }, timeout/2)
+            })
         </script>
-</section>
+    </section>
 
 </view>
